aaf8b2c1401324c55ed316f676076ef5ab8146fe
jallowaminatou@yahoo.com <jalllowaminatou@yahoo.com>
2024-07-21 22:21:48 -0400

    final push

---
+++ main.js
@@ -0,0 +1,38 @@
+//main.js
+
+import { fetchCocktail } from './api.js';
+import { renderCocktail, displayError, createCocktail, updateCocktail, deleteCocktail } from './dom.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cocktailContainer = document.getElementById('cocktail-container');
+  const createButton = document.getElementById('create-cocktail');
+  const updateButton = document.getElementById('update-cocktail');
+  const deleteButton = document.getElementById('delete-cocktail');
+
+  let cocktails = [];
+  let currentCocktail = null;
+
+  const loadCocktail = async () => {
+    try {
+      const cocktail = await fetchCocktail();
+      currentCocktail = cocktail;
+      const cocktailImage = renderCocktail(cocktailContainer, cocktail);
+      cocktailImage.addEventListener('click', loadCocktail);
+    } catch (error) {
+      displayError(cocktailContainer, 'Sorry, we couldn\'t load a cocktail at this time.');
+    }
+  };
+
+  // Initial fetch
+  loadCocktail();
+
+  // Event listeners for buttons
+  createButton.addEventListener('click', () => createCocktail(cocktails));
+  updateButton.addEventListener('click', () => updateCocktail(currentCocktail));
+  deleteButton.addEventListener('click', () => {
+    cocktails = deleteCocktail(cocktails, currentCocktail, cocktailContainer);
+  });
+});
+
+  
+  
